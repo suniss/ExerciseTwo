66608c2eba1674089554cc228a981d3f3659de0c
sunisakc <sunisa.kan@gmail.com>
2021-06-16 10:02:59 +0200

    Add project files.

---
+++ ExerciseTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciseTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
