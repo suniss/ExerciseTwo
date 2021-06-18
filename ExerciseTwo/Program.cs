8984d0e0658de6266589faf77db4293e2914815c
sunisakc <sunisa.kan@gmail.com>
2021-06-18 13:20:25 +0200

    Finished

--- ExerciseTwo/Program.cs
+++ ExerciseTwo/Program.cs
@@ -6,13 +6,13 @@ namespace ExerciseTwo
     {
         static void Main(string[] args)
         {
-            MainMunu(); 
+            MainMunu();
         }
         public static void MainMunu()
         {
             // display menu for user to choose
             bool DisplayMenu = true;
-            while(DisplayMenu)
+            while (DisplayMenu)
             {
                 Console.Clear();
                 Console.WriteLine("Welcome to main menu:)");
@@ -22,7 +22,7 @@ namespace ExerciseTwo
                 Console.WriteLine("Enter 3 to find the third word");
                 Console.WriteLine("Enter 0 to exit menu");
                 string input = Console.ReadLine();
-                switch(input)
+                switch (input)
                 {
                     case "0":
                         DisplayMenu = false;
@@ -50,8 +50,36 @@ namespace ExerciseTwo
 
         private static void MovieTicket()
         {
+            Console.Clear();
+            Console.WriteLine("This program will print out your movie ticket price");
+            Console.Write("Number of person to buy:");
+            uint number; // number of person to buy
+            int total = 0;
+            string input;
+            byte age; //get age 
+            if (UInt32.TryParse(Console.ReadLine(), out number))
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    Console.Write("Please enter the age of person" + (i + 1).ToString() + " ");
+                    input = Console.ReadLine();
+                    if (!Byte.TryParse(input, out age))
+                    {
+                        Console.WriteLine("Invalid age, please try again");
+                        i--;
+                    }
+                    else
+                        total += Price(age);
 
+                }
+                Console.WriteLine("-----Total price--------");
+                Console.WriteLine("{0:C}", total);
+            }
+            else
+                Console.WriteLine("Incorrect number of persons");
+            Console.ReadLine();
         }
+    
         private static void PrintString() // Print string ten times
         {
             Console.Clear();
@@ -72,16 +100,48 @@ namespace ExerciseTwo
             Console.Clear();
             Console.WriteLine("Please enter string with minimum 3 words:");
             string input = Console.ReadLine();
+            input = input.Trim();
 
             // split string to seperate word
-            char[] delimiterChars = new char[] { };
+            char[] delimiterChars = new char[] { ' ', ',', '.', ':', '\t', ';' };
             string[] array = input.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
-            
+
             while (array.Length < 3)
             {
-               
+                Console.WriteLine("Your input is less than 3 words, please try again");
+                input = Console.ReadLine();
+                array = input.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
             }
+                Console.WriteLine("The third words is:" + array[2]);
+                Console.ReadLine();
+            }
+        private static int Price(byte age)
+        {
+            int price = 0;
+            if(age <= 2 || age > 100)
+            {
+                Console.WriteLine("Free ticket");
+                price = 0;
+            }
+            else if (age < 20)
+            {
+                Console.WriteLine("Kids price: 80kr");
+                price = 80;
+
+            }
+            else if (age > 64)
+            {
+                Console.WriteLine("Senior Discount: 90kr");
+                 price = 90;
+            }
+            else
+            {
+                Console.WriteLine("Standard price: 120kr");
+                price = 120;
+            }
+            return price;
+        }
         }
         
     }
-}
+
